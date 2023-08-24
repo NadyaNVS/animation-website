ae9f68bcf7dacf4a8fdaafbc79d90c5eadb41c4b
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2023-08-25 00:58:56 +0300

    finish

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const expandPill = document.querySelector("#expand");
 const xLetterPath = document.querySelector("#x-letter");
 const xLetterSVG = document.querySelector("#x-letter-svg");
 const xBox = document.querySelector(".x-box");
-const socialPills = document.querySelector(".social-fan");
 const reversePill = document.querySelector("#reverse");
 const boxContainer = document.querySelector(".box-container");
 const iconPath = document.querySelector("#icon-rotate");
@@ -54,3 +53,125 @@ const reverse = () => {
 };
 
 reversePill.addEventListener("click", reverse);
+
+const addTheme = (
+  bodyBackgroundColor,
+  strokeWidth,
+  svgFill,
+  opacity,
+  lineColor,
+  borderRadius,
+  boxBackgrounColor,
+  pillBackgroundColor
+) => {
+  body.style.backgroundColor = bodyBackgroundColor;
+  xLetterPath.style.strokeWidth = strokeWidth;
+  xLetterPath.style.stroke =
+    lineColor || colorPalettes[paletteIndex][xLetterIndex].altStroke;
+  xLetterSVG.style.fill =
+    svgFill || colorPalettes[paletteIndex][xLetterIndex].fill;
+  xLetterSVG.style.opacity = opacity;
+  xBox.style.backgroundColor =
+    boxBackgrounColor || colorPalettes[paletteIndex][xLetterIndex].fill;
+  iconPath.style.stroke =
+    lineColor || colorPalettes[paletteIndex][rotateIconIndex].altStroke;
+  iconPath.style.strokeWidth = strokeWidth;
+
+  allBoxes.forEach(
+    (box, i) =>
+      (box.style.backgroundColor =
+        boxBackgrounColor || colorPalettes[paletteIndex][i].fill)
+  );
+
+  allPills.forEach((pill, i) => {
+    pill.style.opacity = opacity;
+    pill.style.backgroundColor =
+      pillBackgroundColor || colorPalettes[paletteIndex][i].fill;
+    pill.style.borderWidth = strokeWidth;
+    pill.style.borderColor =
+      lineColor || colorPalettes[paletteIndex][i].altStroke;
+    pill.style.borderBlockStyle = "solid";
+    pill.style.borderRadius = borderRadius;
+  });
+
+  allHiddenPills.forEach((hiddenPill) => {
+    hiddenPill.style.opacity = opacity;
+    hiddenPill.style.borderWidth = strokeWidth;
+    hiddenPill.style.backgroundColor =
+      pillBackgroundColor || colorPalettes[paletteIndex][socialFanIndex].fill;
+    hiddenPill.style.borderColor =
+      lineColor || colorPalettes[paletteIndex][socialFanIndex].altStroke;
+    hiddenPill.style.borderRadius = borderRadius;
+  });
+
+  allArrows.forEach((arrow) => {
+    arrow.style.borderBlockStyle = "solid";
+    arrow.style.borderColor = lineColor;
+    arrow.style.borderWidth = "0 " + strokeWidth + " " + strokeWidth + " 0";
+    arrow.style.opacity = opacity;
+  });
+};
+
+const moveSlider = () => {
+  const sliderInput = document.querySelector("#slider-input");
+  const sliderValue = sliderInput.value;
+
+  if (sliderValue == 0) {
+    // bodyBackgroundColor,strokeWidth,svgFill,opacity,lineColor,borderRadius,boxBackgrounColor,pillBackgroundColor
+    addTheme(
+      "white",
+      "12px",
+      null,
+      1,
+      "rgb(38, 38, 38)",
+      "100px",
+      "white",
+      null
+    );
+  }
+  if (sliderValue > 1 && sliderValue <= 3) {
+    addTheme("white", "2px", "white", 0.5, null, "75px", null, "white");
+  }
+  if (sliderValue >= 4 && sliderValue <= 6) {
+    addTheme("white", "1px", "white", 0.5, null, "90px", null, "white");
+  }
+  if (sliderValue >= 7 && sliderValue < 9) {
+    addTheme(
+      "white",
+      "2px",
+      "white",
+      0.5,
+      "rgb(38,38,38)",
+      "50px",
+      null,
+      "white"
+    );
+  }
+  if (sliderValue == 10) {
+    addTheme(
+      "rgb(38,38,38)",
+      "12px",
+      "white",
+      1,
+      "black",
+      0,
+      "transparent",
+      "white"
+    );
+  }
+  console.log(sliderValue);
+};
+sliderInput.addEventListener("input", moveSlider);
+
+const changePalette = () => {
+  xLetterPath.classList.add("pulse");
+  if (paletteIndex >= 2) {
+    paletteIndex = 0;
+  } else {
+    paletteIndex++;
+  }
+  moveSlider();
+  setTimeout(() => xLetterPath.classList.remove("pulse"), 500);
+};
+
+xBox.addEventListener("click", changePalette);
